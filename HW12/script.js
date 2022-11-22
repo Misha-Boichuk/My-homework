bae5bda12e35c0148aa008abb8c2dde21cd840d8
Mykhailo Boichuk <bastenpro88@gmail.com>
2022-11-22 11:17:26 +0200

    add chenge words and add finich console.log

--- HW12/script.js
+++ HW12/script.js
@@ -19,9 +19,13 @@ winter = sports.slice(0, 5);
 fruits = fruits.concat(summer.splice(2, 2));
 fruits = fruits.concat(winter.splice(2, 1));
 
-for (i = 0; i < summer.lenght; i++) {
+for (i = 0; i < summer.length; i++) {
     if (summer[i] [0] === 'skier') {
         console.log("*** Winter sports ***");
     }
     console.log(summer[i].join(": "));
+	if(summer[i] [0] === "swimmer") {
+		console.log("***Summer sports***");
+	}
 }
+console.log(fruits);
