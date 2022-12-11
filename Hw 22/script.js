ac939817f4b9ee26c5d6bc1b087a384e370ee339
Mykhailo Boichuk <bastenpro88@gmail.com>
2022-12-11 20:22:48 +0200

    add function convert and loops for new object

--- Hw 22/script.js
+++ Hw 22/script.js
@@ -3,10 +3,35 @@
 // Функция должна вернуть новый объект вида:
 
 
-// newObj = {
-//     x: 20,
-//     y: 20,
-//     z: 30,
-//     k: 23,
-//     p: 13
-// }
+let obj = {
+    x: 10,
+    y: 20,
+    inner: {
+        x: 20,
+        z: 30,
+    },
+    foo2: {
+        k: 23,
+        p: 13,
+        l: [100, 200, 300]
+    }
+}
+
+function convert(object) {
+    let newObj = {};
+
+    for(let key in object) {
+        if(typeof object[key] === "object") {
+            const newObj2 = convert(object[key]);
+            for(let key in newObj2) {
+                newObj[key] = newObj2[key];
+            }
+        } else {
+            newObj[key] = object[key];
+        }
+    }
+
+    return newObj;
+}
+
+console.log(convert(obj));
