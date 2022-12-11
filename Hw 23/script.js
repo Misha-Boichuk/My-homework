a14d97181edc50978ef9172945081d1533cb7515
Mykhailo Boichuk <bastenpro88@gmail.com>
2022-12-11 21:04:25 +0200

    changed fuction assign for this method

--- Hw 23/script.js
+++ Hw 23/script.js
@@ -8,3 +8,40 @@
 // assignObjects({ x: 10, y: 20 }, { z: 30 }) вывод { x:10, y:20, z: 30 }
 // assignObjects({ x: 10 }, { x: 20, y: 30 }) вывод { x:20, y: 30 }
 
+// let objFirst = { x: 10, y: 20};
+// let objSecond = {y: 100, z: 30};
+const obj1= {
+    x: 10,
+    y: 20,
+};
+
+const obj2 = {
+    z: 30,
+};
+
+const objA = {
+    x: 10,
+};
+
+const objB = {
+    x: 20,
+    y: 30,
+}
+
+function assignObject(objA, objB){
+    let newObj = {};
+
+    for( let key in objA) {
+        newObj[key] = objA[key];
+    }
+    for(let key in objB) {
+        newObj[key] = objB[key];
+    }
+
+    return newObj;                  //Object.assign({}, obj1, obj2);
+}
+
+console.log(assignObject(obj1, obj2));
+console.log(assignObject(objA, objB));
+
+//console.log(assignObject(objFirst, objSecond));
