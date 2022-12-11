423f1176dd8a50a43668c8fea480013f5adea931
Mykhailo Boichuk <bastenpro88@gmail.com>
2022-12-11 18:32:47 +0200

    cteated new folder

--- Hw 13/script.js
+++ Hw 13/script.js
@@ -12,18 +12,32 @@
 // Внешний вид задания НЕ важен, важен только JS-код))
 
 // Chess
-
 TRs = [];
+alphabet = "abcdefgh";
+figures = ["rook", "horse", "bishop", "queen", "king"];
+figures = figures.concat(figures.slice(0,3).reverse());
 
-for(tr = 1; tr <= 10; tr++) {
+for(tr = 10; tr >= 1; tr--) {
+    color = tr >= 4 ? "black" : "white";
     TDs = [];
 
     for(td = 1; td <= 10; td++) {
-        TDs.push(`<td></td>`)
+        data = "";
+        if((tr === 1 || tr === 10) && td !== 1 && td !== 10) {
+            data = alphabet[td -2];
+        }
+        if((td === 1 || td === 10) && tr !== 1 && tr !== 10) {
+            data = tr - 1;
+        }
+        if((tr === 3 || tr === 8) && td !== 1 && td !== 10) {
+            data = `<img src="images/chass/${color}/pawn.svg">`
+        }
+        if((tr === 2 || tr === 9) && td !== 1 && td !== 10) {
+            data = `<img src="images/chass/${color}/${figures[td-2]}.svg">`
+        }
+      
+        TDs.push(`<td>${data}</td>`)
     }
-    TRs.push (`<tr>${TDs.join("")}</tr>`)
+    TRs.push(`<tr>${TDs.join("")}</tr>`)
 } 
-
 document.write(`<table class="chess">${TRs.join("")}</table>`);
-
-
