9840550d925a197f1d7cc33a08e093446ef70c56
Mykhailo Boichuk <bastenpro88@gmail.com>
2022-12-13 19:03:55 +0200

    changed image faile

--- Hw 13/script.js
+++ Hw 13/script.js
@@ -12,6 +12,26 @@
 // Внешний вид задания НЕ важен, важен только JS-код))
 
 card = [];
-number [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "T"];
+number = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "T"];
 suit = [`clubs`, `spades`, `diamond`, `hearts`];
-icon
+icon = [`jack`, `queen`, `king`, `clubs`];
+image = "";
+br = 9;
+
+for(let j = 0; j < 13; j++) {
+    if (j === br) {
+        br++;
+        image = `<img class="person" src="images/${icon[br - 10]}.svg" alt="${icon[br - 10]}"></img`;
+    }
+    for ( let i = 0; i < 4; i++) {
+        if(j === 12) {
+            image = `<img class="person" src="images/${suit[i]}.svg" alt="${suit[i]}></img>`;
+        }
+        card.push(`
+        <div class="card">
+           <div class="card_info">${number[j]}<img src="images/${suit[i]}.svg" alt="${suit[i]}"></div>${image}
+           <div class="card_info">${number[j]}<img src="images/${suit[i]}.svg" alt="${suit[i]}"></div> 
+        </div>`)
+    }
+}
+document.write(`<div class="wrapper">${card.join("")}</div>`)
