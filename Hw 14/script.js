8be07204a8d1e1472e24136ea2384cf6afa911fb
Mykhailo Boichuk <bastenpro88@gmail.com>
2022-12-24 18:07:33 +0200

    changed function and added methoud splice, finuish Hw 14

--- Hw 14/script.js
+++ Hw 14/script.js
@@ -8,18 +8,11 @@
 // console.log(array);
 // Результат: [1, 2, 3, 4, 6, 7]
 
- const array = [1, 2, 3, 4, 5, 6, 7];
-// function removeElement(array, item) {
-//      console.log(array);
-// }
+const array = [1, 2, 3, 4, 5, 6, 7];
 
-// removeElement(array.splice(0, 5))
+function removeElement() {
+      array.splice(4, 1)
+      console.log(array);
+}
 
-
-console.log(array);
-
-let modified = array.filter(function(value){
-    return value !== 4;
-}) 
-
-console.log(modified);
+removeElement(array);
