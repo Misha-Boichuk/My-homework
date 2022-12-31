bf6270368a13c912c4d51e580e24636783303919
Mykhailo Boichuk <bastenpro88@gmail.com>
2022-12-31 10:44:20 +0200

    created constructor supermath

--- Hw 28/script.js
+++ Hw 28/script.js
@@ -1,15 +1,56 @@
-//          ДЗ 28. SuperMath
+//    ДЗ 28. SuperMath
 // Створити клас SuperMath.
-
-// Додати до екземпляра метод - check(obj), параметр obj якого має властивості X, Y, znak. Метод повинен підтвердити у користувача, чи хоче він зробити дію znak c Х і У. Якщо хоче, зробити математичну дію znak (яка описана в прототипі), інакше - запитати введення нових даних через метод input() класу SuperMath.
-
-
-
+// Додати до екземпляра метод - check(obj), параметр obj якого має властивості X, Y, znak.
+// Метод повинен підтвердити у користувача, чи хоче він зробити дію znak c Х і У.
+// Якщо хоче, зробити математичну дію znak (яка описана в прототипі), інакше - запитати введення 
+// нових даних через метод input() класу SuperMath.
 // Приклад об'єкта: `obj = {X:12, Y:3, znak: “/”}`, можливі варіанти znak => `+ - / * %`.
-
 // При введенні znak потрібно перевірити коректність введення на можливі математичні дії.
-
-
 
 // p = new SuperMath();
 // p.check(obj); // --> no p.input() -> 3 prompt -> рахує
+
+const OPERATIONS = {
+    "+": function(x, y) { return x + y},
+    "-": function(x, y) { return x - y},
+    "*": function(x, y) { return x * y},
+    "/": function(x, y) { return x / y},
+};
+
+class SuperMath {
+    constructor() {
+        this.input()
+    }
+
+    input() {
+        do {
+           this.znak = +prompt(`Enter x`)
+        } while(isNaN(this.x));
+        do {
+           this.znak = +prompt(`Enter y`)
+        } while(isNaN(this.y));
+        do {
+           this.znak = +prompt(`Enter znak: ${this.getOperations()}`)
+        } while(!OPERATIONS[this.znak]);
+    } 
+
+    check() {
+        let userApproved = confirm(`Do you want make operation ${this.x} ${this.y} ${this.znak}`)
+        this.input();
+        return OPERATIONS[this.znak](this.x, this. y);
+    }
+
+    getOperations() {
+        let operands = [];
+        for(let key in OPERATIONS) {
+           operands.push(key);
+        }
+
+        return operands.join(", ")
+    }
+
+}
+
+const obj = new SuperMath(10, 20, "+")
+
+console.log(obj.check());
