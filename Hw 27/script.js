f6b9c09e7bd2185040b21cb490ae304debae2847
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-01-24 18:53:38 +0200

    added return this

--- Hw 27/script.js
+++ Hw 27/script.js
@@ -27,3 +27,20 @@
 
 // ladder.up().up().down().showStep(); // 1
 // Такий підхід широко використовується в бібліотеках JavaScript.
+
+let ladder = {
+        step: 0,
+        up: function () {
+            this.step++;
+            return this;
+        },
+        down: function () {
+            this.step--;
+            return this;
+        },
+        showStep: function () { 
+            alert(this.step);
+            return this;
+        }
+    };
+    ladder.up().up().down().showStep(); 
