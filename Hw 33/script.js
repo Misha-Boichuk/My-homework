ab4d7189ab985960f6c2820a96e2a96fa31af915
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-01-27 23:31:33 +0200

    added html tr and td, js added body and table append

--- Hw 33/script.js
+++ Hw 33/script.js
@@ -1,2 +1,16 @@
 //ДЗ 33. Таблиця
 // Вивести таблицю 10 × 10, заповнену числами від 1 до 100 (таблиця створюється динамічно)
+
+const body = document.querySelector("body");
+const table = document.createElement("table");
+
+for (let i = 0; i < 10; i++) {
+    const tr = document.createElement("tr");
+    for (let j = 0; j < 10; j++) {
+        const td = document.createElement("td");
+        td.innerHTML = i * 10 + j + 1;
+        tr.append(td); 
+    }
+    table.append(tr);
+}
+body.append(table);
