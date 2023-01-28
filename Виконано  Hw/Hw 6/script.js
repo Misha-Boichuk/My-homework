136a581d6deed999bbd5e458b179a56835abafdd
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-01-28 13:04:35 +0200

    created new folder

---
+++ Виконано  Hw/Hw 6/script.js
@@ -0,0 +1,18 @@
+//      ДЗ 6. SIMPLE QUIZ
+
+// You need to make simple quiz.
+
+// Ask the users a few questions (>4), using prompt.
+// If the answer is right add 10 points (for every right answers).
+// If the answer is wrong or user clicked cancel - you don't add any points.
+// When the user will answer all questions, show the result using alert (like: You have 30 points. You answered correctly for 3/10.).
+
+// Some question for example:
+
+// How much will 2+2?
+// The sun rises in the east?
+// How much will 5 / 0 be?
+// What color is the sky?
+// What is the correct answer to the ultimate question of life, the universe and all that. (42)
+
+
