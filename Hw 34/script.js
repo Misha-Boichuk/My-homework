0b10fe4cce1b42ab0120d634328feb537cfeea1a
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-02-02 15:16:54 +0200

    aded foto

--- Hw 34/script.js
+++ Hw 34/script.js
@@ -1,3 +1,12 @@
-// ДЗ 34. Виведення зображень
+//       ДЗ 34. Виведення зображень
 // У папці images є зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5 .jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg 
 // Вивести зображення з цієї папки, отримане випадковим чином (Math.random)
+
+
+// image = document.createElement('img');
+// image.src = `image/${Math.floor(Math.random() * 9 + 1)}.jpg`;   
+// document.body.appendChild(image);
+
+document.body.appendChild(document.createElement('img'))
+.src = `image/${Math.floor(Math.random() * 9 + 1)}.jpg`;
+
