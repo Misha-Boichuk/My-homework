bb5fb7362f87a35e163362f755db14e95651bbb4
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-02-02 14:34:09 +0200

    added input focus

--- Hw 32/script.js
+++ Hw 32/script.js
@@ -3,33 +3,13 @@
 // Під час фокусування на цьому полі збоку з'являється <div>. 
 // При пропажі фокусу - <div> так само пропадає
 
-
-const body = document.body;
-
-const childArr = [...body.children];
-
-console.log(body.children);
-
-const div = document.createElement('div');
-
-div.style.width = '100px';
-div.style.height = '100px';
-div.style.backgroundColor = 'red';
-div.style.position = 'absolute';
-div.style.top = '0';
-div.style.left = '10';
-
-body.appendChild(div);
-const input = document.createElement('input');
-
-input.style.width = '100px';
-
-body.appendChild(input);
+const input = document.querySelector('input');
+const div = document.querySelector('div');
 
 input.addEventListener('focus', () => {
-    div.style.display = 'block';
-});
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
 
-input.addEventListener('blur', () => {
-    div.style.display = 'none';
-});
+    console.log(firstName); // Виводить в консоль значення з полів
+    console.log(lastName);  // Виводить в консоль значення з полів
+});                          //  не знаю чи правильно працює !!!
