bc4bb4ac852b3c7386ff3d979c6013cf1eaab957
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-02-06 10:42:22 +0200

    adde file request

--- My Practick/practik.js
+++ My Practick/practik.js
@@ -4,6 +4,18 @@
 const xml = new XMLHttpRequest();
 xml.open('GET', 'request/fileA.json');
 xml.send();
-console.log(xml);
-let parse = response => JSON.parse(response);
+// console.log(xml);
+// let parse = response => JSON.parse(response);
 
+xml.addEventListener('readystatechange', () => {
+    const  response = parse(xml.response);
+    console.log(xml.readyStage)
+    console.log(response);
+});
+
+//  console.log(parse(xml.responseText));
+
+console.log('Move!');
+for (let i = 0; i  < 10; i++) {
+    console.log(i);
+}
