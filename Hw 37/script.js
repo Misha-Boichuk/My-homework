b6556699294db96c63dc6754f6ca54098447c27a
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-02-11 11:08:59 +0200

    aded function forEach for smile

--- Hw 37/script.js
+++ Hw 37/script.js
@@ -1,20 +1,18 @@
 //ДЗ 37. Голосування
-// Створити програму для відображення результатів голосування. З наступними умовами:
+// Створити програму для відображення результатів голосування.
+// З наступними умовами:
 
-// Як варіанти відповіді - смайлики. За замовчуванням, 5шт.
+//Як варіанти відповіді - смайлики. За замовчуванням, 5шт
 // Виведення даних - смайлики в один рядок, під ними - кількість тих, хто проголосував за кожен смайл.
 // При натисканні на смайл - під ним змінюється значення лічильника.
 // Реалізувати таким чином, щоб додавання нових варіантів відповіді не вело до додавання нових функцій.
 
+
 const smile = document.querySelectorAll('.smile');
-const counter = document.querySelectorAll('.counter');
-const smileArr = Array.from(smile);
-const counterArr = Array.from(counter);
 
-smileArr.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        counterArr[index].textContent++;
-    })
-})
-
-
+smile.forEach(el => {
+    let counter = 1;
+    el.addEventListener('click', (e) => {
+        e.target.nextElementSibling.innerText = counter++;
+    });
+});
