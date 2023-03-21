9dac6e73047c753fc83244ca5d4d43e407cbee93
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-03-21 23:12:23 +0200

    aded function and finish worked

---
+++ Hw 48/src/calculator/calculator.js
@@ -0,0 +1,8 @@
+const add = require('./add');
+const sub = require('./sub');
+const mul = require('./mul');
+const div = require('./div');
+
+const calculator = { add, sub, mul, div };
+
+module.exports = calculator;
---
+++ Hw 48/src/index.js
@@ -0,0 +1,6 @@
+const calculator = require('./calculator/calculator');
+
+console.log(calculator.add(10, 2));
+console.log(calculator.sub(10, 2));
+console.log(calculator.mul(10, 2));
+console.log(calculator.div(10, 2));
