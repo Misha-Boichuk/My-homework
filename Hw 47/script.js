b24a05c9a4aaa383afe7c0dade63de1ab5256a7a
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-03-22 00:35:57 +0200

    aded fetch and worked new heroes

--- Hw 47/script.js
+++ Hw 47/script.js
@@ -30,3 +30,233 @@
 //   При изменении состояния checkbox в колонке Favourite происходит изменение данных по этому персонажу в базе (PUT).
 //   При нажатии на кнопку Delete в строке персонажа происходит удаление с базы соответствующего героя (DELETE) и удаление соответствующей tr с таблицы.
 
+//const  Api = 'https://640e2c43b07afc3b0dc12d44.mockapi.io';
+// const  table = document.querySelector('#heroesTable');
+// const  form = document.querySelector('#heroesForm');
+// const  name = document.querySelector('#heroName');
+// const  comics = document.querySelector('#heroComics');
+// const  favourite = document.querySelector('input[type="checkbox"]');
+// const  btn = document.querySelector('#Btn');
+
+// const  getHeroes = async () => {
+//       const  response = await fetch(`${Api}/heroes`);
+//       const  data = await response.json();
+//       return  data;
+//       };
+
+// const  getUniverses = async () => {
+//       const  response = await fetch(`${Api}/universes`);
+//       const  data = await response.json();
+//       return  data;
+//       };
+
+// const  createHero = async (hero) => {
+//       const  response = await fetch(`${Api}/heroes`, {
+//             method: 'POST',
+//             headers: {
+//                   'Content-Type': 'application/json;charset=utf-8',
+//                   'Access-Control-Allow-Origin': '*',
+//             },
+//             body: JSON.stringify(hero),
+//       });
+//       const  data = await response.json();
+//       return  data;
+//       };
+
+// const  updateHero = async (hero) => {
+//       const  response = await fetch(`${Api}/heroes/${hero.id}`, {
+//             method: 'PUT',
+//             headers: {
+//                   'Content-Type': 'application/json;charset=utf-8',
+//                   'Access-Control-Allow-Origin': '*',
+//             },
+//             body: JSON.stringify(hero),
+//       });
+//       const  data = await response.json();
+//       return  data;
+//       };
+
+// const  deleteHero = async (id) => {
+//       const  response = await fetch(`${Api}/heroes/${id}`, {
+//             method: 'DELETE',
+//       });
+//       const  data = await response.json();
+//       return  data;
+//       };
+
+// const  renderHeroes = async () => {
+//       const  heroes = await getHeroes();
+//       const  universes = await getUniverses();
+//       const  heroesList = heroes.forEach((hero) => {
+//             const  universe = universes.find((item) => item.id === hero.comics);
+//             hero.comics = universe.name;
+//             return  `<tr>
+//             <td>${hero.name}</td>
+//             <td>${hero.comics}</td>
+//             <td><input type="checkbox" data-id="${hero.id}" ${hero.favourite ? 'checked' : ''}></td>
+//             <td><button data-id="${hero.id}">Delete</button></td>
+//             </tr>
+//             `;
+//       });
+//       table.innerHTML = heroesList.append();
+//       };
+
+// const  addHero = async (e) => {
+//       e.preventDefault();
+//       const  hero = {
+//             name: name.value,
+//             comics: comics.value,
+//             favourite: favourite.checked,
+//       };
+//       const  heroes = await getHeroes();
+//       const  heroExist = heroes.find((item) => item.name === hero.name);
+//       if (heroExist) {
+//             return  console.log('Hero with this name already exist');
+//             };
+//       await createHero(hero);
+//       renderHeroes();
+//       };
+
+// const  updateFavourite = async (e) => {
+//       if (e.target.tagName !== 'INPUT') {
+//             return  console.log('Not input');
+//             };
+//       const  hero = {
+//             id: e.target.dataset.id,
+//             favourite: e.target.checked,
+//       };
+//       await updateHero(hero);
+//       renderHeroes();
+//       };
+
+// const  deleteHeroById = async (e) => {
+//       if (e.target.tagName !== 'BUTTON') {
+//             return  console.log('Not button');
+//             };
+//       await deleteHero(e.target.dataset.id);
+//       renderHeroes();
+//       }
+
+// renderHeroes();
+// form.addEventListener('submit', addHero);
+// table.addEventListener('click', updateFavourite);
+// table.addEventListener('click', deleteHeroById);
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+              //21.03.2023   23 59 59
+
+
+
+fetch('https://640e2c43b07afc3b0dc12d44.mockapi.io/universes')
+    .then(response => response.json())
+    .then(universes => {
+
+  const comicsSelect = document.getElementById('hero-comics');
+        universes.forEach(universe => {
+  const option = document.createElement('option');
+        option.value = universe.name;
+        option.textContent = universe.name;
+        comicsSelect.append(option);
+    });
+});
+
+const form = document.getElementById('add-hero-form');
+      form.addEventListener('submit', e => {
+         e.preventDefault();
+const name = document.getElementById('hero-name').value;
+const surname = document.getElementById('hero-surname').value;
+const comics = document.getElementById('hero-comics').value;
+const favorite = document.getElementById('hero-favorite').checked;
+
+fetch('https://640e2c43b07afc3b0dc12d44.mockapi.io/heroes', {
+     method: 'POST',
+     headers: {
+               'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+                            name,
+                            surname,
+                            comics,
+                            favorite
+                         })
+                    })
+     .then(response => {
+          if (response.ok) {
+              return response.json();
+          } else if (response.status === 409) {
+             console.log(`Hero with name ${name} already exists`);
+             throw new Error(`Hero with name ${name} already exists`);
+          } else {
+             throw new Error('Failed to add hero');
+        }
+      })
+     .then(hero => {
+const row = document.createElement('tr');
+      row.dataset.id = hero.id;
+
+const nameCell = document.createElement('td');
+      nameCell.textContent = hero.name;
+           
+          row.append(nameCell);
+
+const surnameCell = document.createElement('td');
+      surnameCell.textContent = hero.surname;
+      row.append(surnameCell);
+
+const comicsCell = document.createElement('td');
+      comicsCell.textContent = hero.comics;
+      row.append(comicsCell);
+
+const favoriteCell = document.createElement('td');
+const favoriteCheckbox = document.createElement('input');
+      favoriteCheckbox.type = 'checkbox';
+      favoriteCheckbox.checked = hero.favorite;
+      favoriteCheckbox.addEventListener('change', () => {
+           fetch(`https://640e2c43b07afc3b0dc12d44.mockapi.io/heroes/${hero.id}`, {
+                  method: 'PUT',
+                  headers: {
+                        'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                        favorite: favoriteCheckbox.checked
+                  })
+           })
+      .then(response => {
+          if (!response.ok) {
+           throw new Error('Failed to update hero');
+           }
+      })
+      .catch(error => console.error(error));
+});
+
+      favoriteCell.append(favoriteCheckbox);
+      row.append(favoriteCell);
+
+const deleteCell = document.createElement('td');
+const deleteButton = document.createElement('button');
+
+  deleteButton.textContent = 'Delete';
+  deleteButton.addEventListener('click', () => {
+      fetch(`https://640e2c43b07afc3b0dc12d44.mockapi.io/${hero.id}`, {
+            method:  'DELETE'   
+       })
+        .then(response => {
+             if (!response.ok) {
+               throw new Error('Failed to delete hero');
+           }
+      })
+        .then(() => row.remove())
+        .catch(error => console.error(error));
+   });
+
+deleteCell.append(deleteButton);
+row.append(deleteCell);
+
+const table = document.getElementById('heroes-table');
+      table.append(row);
+})
+      .catch(error => console.error(error));
+});
