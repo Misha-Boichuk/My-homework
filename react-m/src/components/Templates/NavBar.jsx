819c7ceb93cf38f4be43ff52e7f5638ef181b0d9
Mykhailo Boichuk <bastenpro88@gmail.com>
2023-04-13 22:56:15 +0300

    fixed files Navbar up

--- react-m/src/components/Templates/NavBar.jsx
+++ react-m/src/components/Templates/NavBar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { styled, Box, Typography } from '@mui/material';
+import {
+  styled,
+  Box,
+  Typography,
+  TextField,
+} from '@mui/material';
 
 const Wrap = styled(Box)(() => ({
   backgroundColor: 'rgb(111,135,255)',
@@ -9,12 +14,18 @@ const Wrap = styled(Box)(() => ({
 
 export default function NavBar() {
   return (
-    <Wrap>
+    <Wrap className='wrap' >
+      <Typography variant='h5' component='h5'>
+        <Link to='/tripmydream/home'>HomePage</Link><br />
+      <TextField
+        type='text'
+        placeholder='Search'
+      />
+      </Typography><br />
       <Typography variant='h4' component='h4'>
-        <Link to='/'>HomePage</Link><br />
         <Link to='/tripmydream/quiz'>Quiz</Link><br />
         <Link to='/tripmydream/create'>CreateQuiz</Link><br />
-        <Link to='/tripmydream/favorite'>Favorite</Link>
+        <Link to='/tripmydream/favorite'>FavoriteQuizzes</Link>
       </Typography>
     </Wrap>
   );
